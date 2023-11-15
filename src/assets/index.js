9f96a7c5f3812d1c7cff077731fdd9882241227b
Pradip Sable <sablepradipsubhash@gmail.com>
2023-11-15 19:51:44 +0530

    mini mistake

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,7 @@
 export { default as Cimg1 } from "./CaurasalImg1.jpeg";
 export { default as Cimg2 } from "./CaurasalImg2.jpeg";
 export { default as Cimg3 } from "./CaurasalImg3.jpeg";
-export { default as Cimg4 } from "./CaurasalImg4.jpeg";
+export { default as Cimg4 } from "./Caurasalimg4.jpeg";
 export { default as Cimg5 } from "./CaurasalImg5.jpeg";
 export { default as bgImg } from "./backgroundimg1.jpeg";
 export { default as logo } from "./Hotel_Logo.jpeg";
